1f8304ecb042b34e19dbd50f7493b0024485889b
Carlos Severo <severo.e.carlos@gmail.com>
2021-01-19 11:11:51 -0300

    setup initial of project, including configuration of alias to facilitate imports, configuration of jest and first test

---
+++ server/src/utils/modules-atlas.ts
@@ -0,0 +1,11 @@
+import * as path from "path";
+import moduleAlias from "module-alias";
+
+//arquivo criado para definidir os alias, para que
+
+const files = path.resolve(__dirname, "../..");
+
+moduleAlias.addAliases({
+  "@src": path.join(files, "src"),
+  "@test": path.join(files, "test"),
+});
