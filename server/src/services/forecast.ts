bc0d3aa0ec01331e6cb3953c0b409b2c1488c41e
kissinger156 <severo.e.carlos@gmail.com>
2021-01-27 13:41:34 -0300

    forecast service with annotations

--- server/src/services/forecast.ts
+++ server/src/services/forecast.ts
@@ -25,6 +25,7 @@ export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
 export class Forecast {
   constructor(protected stormGlass = new StormGlass()) {}
 
+  //funcao ira listar uma promisse com o type TimeForecast, ira receber primeiro uma praia, com suas proriedades e depois de listar os dados da api, ser feito um map no point (variavel que recebeu os dados externos) e dentro dele ser adicionado os dados da praia, para ficar no type BeachForecast
   public async processForecastForBeaches(
     beaches: Beach[]
   ): Promise<TimeForescast[]> {
@@ -47,6 +48,7 @@ export class Forecast {
     return this.mapForecastByTime(pointsWithCorrectSources);
   }
 
+  //funcao para organizar a listagem por forecaast inside of time basicamente esse função está sendo chamada no metodo que busca os dados na api externa, criamos uma lista vazia primeiramente e depois um for nos dados que ira ser passado para a funcao, o for ir verificar basicamente se o time dos dados passados existe e é igual ao da lista vazia que criamos, caso seja, vamos apenas dar um timePoint.forecast.push(point) ou seja, vamos apenas adicionar o point dentro do objeto forecast, caso não seja nós vamos normalizar os dados, organizando em time e forecast.
   private mapForecastByTime(forecast: BeachForecast[]): TimeForescast[] {
     const forecastByTime: TimeForescast[] = [];
     for (const point of forecast) {
